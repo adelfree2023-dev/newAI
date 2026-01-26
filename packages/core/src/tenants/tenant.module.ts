20bb12bf1218eb680261aaec1be07d5549a156d2
adelfree2023-dev <adelfree2023@gmail.com>
2026-01-26 18:40:47 +0200

    fix(tenants): register TenantController in TenantModule to fix 404 error

--- packages/core/src/tenants/tenant.module.ts
+++ packages/core/src/tenants/tenant.module.ts
@@ -1,5 +1,6 @@
 import { Module, Global } from '@nestjs/common';
 import { TenantService } from './tenant.service';
+import { TenantController } from './tenant.controller';
 import { TenantConnectionService } from './database/tenant-connection.service';
 import { SchemaInitializerService } from './database/schema-initializer.service';
 import { TenantContextService } from '../security/layers/s2-tenant-isolation/tenant-context.service';
@@ -8,7 +9,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([])], // لا حاجة للكيانات هنا
+  imports: [TypeOrmModule.forFeature([])],
+  controllers: [TenantController],
   providers: [
     TenantService,
     TenantConnectionService,
