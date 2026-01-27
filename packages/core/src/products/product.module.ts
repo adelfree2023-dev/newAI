c1d981614da59dfb0166e805e418db0ba3f9e9d6
adelfree2023-dev <adelfree2023@gmail.com>
2026-01-27 23:55:16 +0200

    feat: definitive repair and forensic alignment for M3 - fixing 404s and 500s

--- packages/core/src/products/product.module.ts
+++ packages/core/src/products/product.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
 import { BusinessController } from './business.controller';
 import { TenantModule } from '../tenants/tenant.module';
 
 @Module({
     imports: [TenantModule],
     providers: [ProductService],
-    controllers: [BusinessController],
+    controllers: [ProductController, BusinessController],
     exports: [ProductService]
 })
 export class ProductModule { }
